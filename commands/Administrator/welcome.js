3c00cc2484e7e174e2a44f714715116e9f0e4660
Koolwiza <72822494+Koolwiza@users.noreply.github.com>
2021-02-15 15:29:22 -0800

    Fixed welcome command

--- commands/Administrator/welcome.js
+++ commands/Administrator/welcome.js
@@ -4,9 +4,9 @@ const Discord = require('discord.js'),
     } = require('../../data/config.json')
 
 module.exports = {
-    name: 'setwelcome',
+    name: 'welcome',
     description: 'Set the welcome channel/message for the server',
-    usage: 'setwelcome <type> [value]',
+    usage: 'welcome <type> [value]',
     aliases: [],
     required: [],
     user: [],
@@ -15,10 +15,13 @@ module.exports = {
     premium: false,
     guildOnly: false,
     async execute(message, args, client) {
+
+        if (!args[0]) return client.missingArgs(message, "Please provide a type.\nchannel | message | show")
+
         let type = args[0].toLowerCase()
 
         if (type === "channel") {
-            let channel = client.resolveChannel(args[1])
+            let channel = await client.resolveChannel(args[1])
             if (!channel) return client.missingArgs(message, "Invalid channel provided/no channel provided")
 
             client.guildData.set(message.guild.id, channel.id, "joinchannel")
@@ -30,39 +33,41 @@ module.exports = {
             }))
 
         } else if (type === "message") {
-            let message = args.slice(1).join(" ")
+            let msg = args.slice(1).join(" ")
 
-            if (!message) return client.missingArgs(message, "Please provide a join message")
+            if (!msg) return client.missingArgs(message, "Please provide a join message")
 
-            client.guildData.set(message.guild.id, message, "joinmessage")
+            client.guildData.set(message.guild.id, msg, "joinmessage")
 
             return message.channel.send(client.baseEmbed(message, {
                 title: "Success",
-                description: "Changed join message to ```" + message + "```",
+                description: "Changed join message to ```" + msg + "```",
                 color: client.colors.green
             }))
         } else if (type === "show") {
             let variables = ""
 
             variables += ` > **Members**
+            > 
             > \`{member:name}\` - The member's name
             > \`{member:mention\`
             > \`{member:tag}\`
             > \`{member:id}\` - The member's id
             > \`{member:createdAt}\` - When the member created his/her account
+            > 
+            > **Server**
             > \`{server:name}\` - The server's name
             > \`{server:members}\` - The server's members`
-
-            let g = await message.guild.members.fetch()
+            
 
             let finJoinMsg = client.guildData.get(message.guild.id).joinmessage
-            .replace(/{member:mention}/g, `<@${message.author.id}>`)
-            .replace(/{member:name}/g, `${message.author.username}`)
-            .replace(/{member:id}/g, `${message.author.id}`)
-            .replace(/{member:tag}/g, `${message.author.tag}`)
-            .replace(/{member:createdAt}/g, `${message.author.createdAt}`)
-            .replace(/{server:name}/g, `${message.guild.name}`)
-            .replace(/{server:members}/g, `${g.size}`)
+                .replace(/{member:mention}/g, `<@${message.author.id}>`)
+                .replace(/{member:name}/g, `${message.author.username}`)
+                .replace(/{member:id}/g, `${message.author.id}`)
+                .replace(/{member:tag}/g, `${message.author.tag}`)
+                .replace(/{member:createdAt}/g, `${message.author.createdAt}`)
+                .replace(/{server:name}/g, `${message.guild.name}`)
+                .replace(/{server:members}/g, `${message.guild.memberCount}`)
 
             let embed = new Discord.MessageEmbed()
                 .setTitle("Welcome settings for " + message.guild.name)
