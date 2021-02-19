d984a110013bce850b27166a521bd87ff4637ed7
Koolwiza <72822494+Koolwiza@users.noreply.github.com>
2021-02-18 20:23:02 -0800

    Added variables and fixed activity

--- events/ready.js
+++ events/ready.js
@@ -17,10 +17,26 @@ module.exports = async client => {
     setInterval(() => {
         if (i > arr.length - 1) i = 0
         let status = config.status[i]
+        /**
+         * Multiple statuses
+         * If you only want one, leave one
+         * Variable templates include:
+         * {client} - Client's username
+         * {guilds} - Client's guilds
+         * {users} - Client's cached users
+         * {roles} - Client's roles
+         * {channels} - Client's channels
+         */
+        let name = status.name
+            .replace(/\{client\}/gi, client.user.username)
+            .replace(/{guilds}/gi, client.guilds.cache.size)
+            .replace(/\{users\}/gi, client.users.cache.size)
+            .replace(/\{roles\}/gi, client.roles.cache.size)
+            .replace(/\{channels\}/gi, client.channels.cache.size)
         client.user.setPresence({
             activity: {
                 type: status.type,
-                name: status.type
+                name: name
             }
         })
         i++
