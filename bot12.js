c41ead6ca639f281cd8ae5bc63913810747af739
Koolwiza <72822494+Koolwiza@users.noreply.github.com>
2021-02-22 09:26:39 -0800

    Added misc events onto client

--- bot12.js
+++ bot12.js
@@ -8,6 +8,12 @@ const Discord = require('discord.js'),
 const Bot12 = require('./src/struct/Bot12Client'),
   client = new Bot12()
 
+
+client.on("disconnect", () => client.logger.warn("Bot is disconnecting..."))
+  .on("reconnecting", () => client.logger.log("Bot reconnecting..."))
+  .on("error", e => client.logger.error(e))
+  .on("warn", info => client.logger.warn(info));
+
 require('./helpers/extenders')
 
 function init() {
