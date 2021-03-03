6b99908d432d3b9ef8447e6ccc6bcecb48b83ddd
Koolwiza <72822494+Koolwiza@users.noreply.github.com>
2021-03-02 17:50:17 -0800

    Require extenders

--- bot12.js
+++ bot12.js
@@ -12,12 +12,13 @@ const Bot12 = require('./src/struct/Bot12Client'),
 client.on("disconnect", () => client.logger.warn("Bot is disconnecting..."))
   .on("reconnecting", () => client.logger.log("Bot reconnecting..."))
   .on("error", e => client.logger.error(e))
-  .on("warn", info => client.logger.warn(info));
+  .on("warn", info => client.logger.warn(info))
 
 async function init() {
   await cmdloader(client, "commands")
   await eventloader(client, "events")
   await docsUpdater(client)
+  await require('./helpers/extenders')
   client.login(client.config.token)
 }
 init()
