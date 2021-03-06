a228dba487d398cb834cbdfcdde372712ba42e98
Koolwiza <72822494+Koolwiza@users.noreply.github.com>
2021-03-06 11:19:02 -0800

    Added buffer if text > 20 chars

--- commands/Fun/ascii.js
+++ commands/Fun/ascii.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js'),
-figlet = require('figlet'),
-util = require('util'),
-fig = util.promisify(figlet)
+  figlet = require('figlet'),
+  util = require('util'),
+  fig = util.promisify(figlet)
 
 module.exports = {
   name: 'ascii',
@@ -11,18 +11,22 @@ module.exports = {
   required: [],
   user: [],
   category: __dirname.split("commands\\")[1],
-  
+
   premium: false,
   guildOnly: false,
   async execute(message, args, client, data) {
-      let text = args.join(" ")
-      if(text.length >= 20) return message.error("Invalid text: \nNo text provided | Text over 20 characters")
+    let text = args.join(" ")
 
-      let ascii = await fig(text).catch(e => {
-          client.logger.error(e)
-          return message.error(e)
-      })
+    let ascii = await fig(text).catch(e => {
+      client.logger.error(e)
+      return message.error(e)
+    })
 
+    if (text.length >= 20) {
+      let buf = Buffer.from(ascii, 'utf-8')
+      return message.channel.send(new Discord.MessageAttachment(buf, args.join(" ").toLowerCase().split(" ").join("-") + ".txt"))
+    } else {
       return message.channel.send("```" + ascii + "```")
+    }
   }
 }
