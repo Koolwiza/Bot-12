68e484531ce3d8f313b1173ff8d675f1a6e5a9ba
Koolwiza <72822494+Koolwiza@users.noreply.github.com>
2021-03-06 11:19:25 -0800

    Tag command :joy:

--- commands/Administrator/tag.js
+++ commands/Administrator/tag.js
@@ -1,4 +1,5 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js'),
+  Tag = require('../../src/struct/Tag')
 
 module.exports = {
   name: 'tag',
@@ -15,58 +16,48 @@ module.exports = {
     if (!type) return message.args("Please provide a type.")
     type = type.toLowerCase()
 
-    let check = (a) => {
-      return (client.tags.find(v => v.guild === message.guild.id && (v.name === a || v.aliases.includes(a))))
+    let check = (tag) => {
+      return (client.tags.find(v => {
+        return v.guild === message.guild.id && (v.tagName === tag || v.aliases.includes(tag))
+      }))
     }
 
     if (["add", "create"].includes(type)) {
       if (!message.member.permissions.has("MANAGE_GUILD") || client.modRole(message, data)) return client.authorPerms(message, ['MANAGE_SERVER'])
       if (!value) return message.args("Please provide a name for the tag")
-      if (client.commands.has(value)) return message.error("There is already a command with this name or alias")
-      if (check(value)) return message.error("There is already a tag with this name or alias")
       if (!args.length) return message.args("Please provide the tag content")
 
       let content = message.attachments.first() ? message.attachments.first().proxyURL : args.join(" ")
 
-      let tagInfo = {
-        name: value.toLowerCase(),
-        guild: message.guild.id,
-        user: message.author.id,
-        reply: content,
-        created: new Date(),
-        aliases: [],
-        modified: null,
-        uses: 0
-      }
+      new Tag(value.toLowerCase(), content, message.author.id, message.guild.id).save()
 
-      client.tags.set(`${message.guild.id}-${value}`, tagInfo)
       return message.sendE("Success", `I have added the tag ${value} with a reply of \`\`\`${await client.clean(args.join(" "))}\`\`\``, client.colors.discord)
 
     } else if (["alias", "alt"].includes(type)) {
       if (!message.member.permissions.has("MANAGE_GUILD") || client.modRole(message, data)) return client.authorPerms(message, ['MANAGE_SERVER'])
- 
+
       if (!value) return message.args("Please provide an option.\nadd | remove")
       let option = args[0]
       if (!check(option)) return message.args("Please provide a valid tag")
       option = option.toLowerCase()
 
+      let alias = args[1]
+    
+      if (!alias) return message.args("Please provide an alias")
+
+
       if (["add", "create"].includes(value)) {
-        let alias = args[1]
-        if (!alias) return message.args("Please provide an alias")
-        client.tags.push(`${message.guild.id}-${option}`, alias, "aliases")
+        Tag.alias("add", `${message.guild.id}-${option}`, alias)
+
         return message.sendE("Success", `I have added the alias \`${alias}\` to the tag \`${alias}\``, client.colors.green)
 
       } else if (["remove", "delete"].includes(value)) {
 
-        let alias = args[1]
-        if (!alias) return message.args("Please provide an alias")
-
-        let b = client.tags.get(`${message.guild.id}-${option}`)
-
-        if (!b.aliases.includes(alias)) return message.error("Tag does not have the alias: " + alias)
-
-        b.aliases.remove(alias)
-        client.tags.set(`${message.guild.id}-${option}`, b)
+        try {
+          Tag.alias("remove", `${message.guild.id}-${option}`, alias)
+        } catch (e) {
+          return message.error("Tag does not have the alias: " + alias)
+        }
 
         return message.sendE("Success", `I have removed the alias \`${alias}\` from the tag \`${alias}\``, client.colors.green)
       }
@@ -74,35 +65,29 @@ module.exports = {
     } else if (['list'].includes(type)) {
 
       let member = await client.resolveUser(value)
-      let guildTags = client.tags.filter(c => c.guild === message.guild.id)
 
-      if (!member) {
-        let tagList = guildTags.map(c => `\`${c.name}\``).join(", ")
-        return message.sendE(`${guildTags.size} Tags`, tagList ? tagList : "There are no tags", client.colors.sky)
-      } else {
-        guildTags = client.tags.filter(c => c.guild === message.guild.id && c.user === member.id)
-
-        let userTagList = guildTags.map(c => `\`${c.name}\``).join(", ")
-
-        return message.sendE(`${member.username}'s Tags`, `**${guildTags.size} Tags**\n${userTagList ? userTagList : "This user has no tags"}`, client.colors.sky)
-
-      }
+      let tags = Tag.list(message, member)
+      let tagList = tags.map(c => `\`${c.name}\``).join(", ")
+      return message.sendE(`${member ? `${member.username}'s` : tags.size} Tags`, `${tagList ? tagList : "There are no tags"}`, client.colors.sky)
 
 
     } else if (['search', 'find'].includes(type)) {
 
-      let foundTags = client.tags.filter(c => c.name.includes(value))
+      let foundTags = Tag.search(value)
       return message.sendE(`${foundTags.size} Tags found`, `${foundTags.map(c => `\`${c.name}\``).join(", ")}`, client.colors.sky)
 
     } else if (['delete', 'remove'].includes(type)) {
       if (!message.member.permissions.has("MANAGE_GUILD") || client.modRole(message, data)) return client.authorPerms(message, ['MANAGE_SERVER'])
-      if(!check(value)) return message.error("Please provide a valid tag")
-      client.tags.delete(`${message.guild.id}-${value}`)
+      if (!check(value)) return message.error("Please provide a valid tag")
+
+      Tag.delete(message, value)
       return message.sendE("Success", `Deleted the tag \`${value}\``, client.colors.green)
-      
+
     } else {
 
       if (check(type)) {
+        
+        client.tags.inc(`${message.guild.id}-${check(type).tagName}`, "uses")
         return message.channel.send(check(type).reply)
       } else {
         return message.error("There is no tag with this name/alias")
