1005fca516f3cfb61341d4b07858478ccbed0083
Koolwiza <72822494+Koolwiza@users.noreply.github.com>
2021-03-06 17:47:44 -0800

    Reaction help menu

--- commands/General/help.js
+++ commands/General/help.js
@@ -14,10 +14,85 @@ module.exports = {
     category: __dirname.split("commands\\")[1],
     premium: false,
     guildOnly: false,
+    ignore: true,
     async execute(message, args, client, data) {
 
         PREFIX = client.guildData.get(message.guild.id, "prefix") ? client.guildData.get(message.guild.id, "prefix") : client.config.defaultSettings.prefix
-        let categories = []
+        if (!args.length) {
+            let categories = {
+
+            }
+            let cat = []
+            const {
+                commands
+            } = client
+
+            commands.forEach(c => {
+                if (!cat.includes(c.category)) cat.push(c.category)
+            })
+
+
+            cat.forEach(cat => {
+                let match = /\d+/g
+                let emojiID = client.emoji.help[cat.toLowerCase()].match(match)[0]
+
+                let tCmds = commands.filter(cmd => cmd.category === cat)
+                categories[emojiID] = tCmds
+            })
+
+
+            let abc = ""
+            cat.forEach(element => {
+                abc += `${client.emoji.help[element.toLowerCase()]}: **${element}**\n`
+            })
+
+            let embed = new Discord.MessageEmbed()
+                .setTitle("Reaction Help Menu")
+                .setAuthor(message.author.tag, message.author.displayAvatarURL())
+                .setDescription(abc)
+                .setFooter(client.user.username, client.user.displayAvatarURL())
+                .setColor(client.colors.sky)
+            let msg = await message.channel.send(embed)
+
+            for (var [k, v] of Object.entries(categories)) {
+                await msg.react(k)
+            }
+
+            let rFilt = (reaction, user) => {
+                return Object.keys(categories).includes(reaction.emoji.id) && user.id === message.author.id
+            }
+
+            let collector = msg.createReactionCollector(rFilt, {
+                time: 5 * 60 * 1000
+            })
+
+            collector.on("collect", async collected => {
+
+                const userReactions = msg.reactions.cache.filter(reaction => reaction.users.cache.has(message.author.id));
+                try {
+                    for (const reaction of userReactions.values()) {
+                        await reaction.users.remove(message.author.id);
+                    }
+                } catch (error) {
+                    client.logger.error('Failed to remove reactions.');
+                }
+
+                let reaction = collected.emoji
+                let content = categories[reaction.id]
+                    .map(em => `\`${em.name} - ${em.description}\``)
+                embed.setDescription(content.join("\n"))
+                msg.edit(embed)
+            })
+
+            collector.on('end', (_, reason) => {
+                if (reason === "time") {
+                    embed.setColor(null)
+                    msg.edit("This message is inactive", embed)
+                }
+            })
+            // Debating if I want to do reaction collector or not
+
+            /* let categories = []
         const {
             commands
         } = message.client
@@ -49,43 +124,44 @@ module.exports = {
                 noArgEmbed.addField(client.emoji.help[cat.toLowerCase()] + ` ${cat} [${tCommands.size}]`, tCommands.map(command => `\`${command.name}\``).join(", "))
             })
             return message.channel.send(noArgEmbed)
-        }
+        } */
+        } else {
+            const name = args[0].toLowerCase();
+            const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
 
-        const name = args[0].toLowerCase();
-        const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
-
-        if (!command) {
-            return message.channel.send({
-                embed: {
-                    author: {
-                        name: message.author.tag,
-                        icon_url: message.author.displayAvatarURL()
-                    },
-                    color: client.colors.red,
-                    description: "I can't find any command named: " + name,
-                    footer: {
-                        text: message.client.user.username,
-                        icon_url: message.client.user.displayAvatarURL()
+            if (!command) {
+                return message.channel.send({
+                    embed: {
+                        author: {
+                            name: message.author.tag,
+                            icon_url: message.author.displayAvatarURL()
+                        },
+                        color: client.colors.red,
+                        description: "I can't find any command named: " + name,
+                        footer: {
+                            text: message.client.user.username,
+                            icon_url: message.client.user.displayAvatarURL()
+                        }
                     }
-                }
-            })
+                })
+            }
+            let msg = ""
+            msg += (`**Name:** ${command.name}\n`);
+
+            if (command.aliases && command.aliases.length !== 0) msg += (`**Aliases:** ${command.aliases.join(", ")}\n`);
+            if (command.description) msg += (`**Description:** ${command.description}\n`);
+            if (command.required && command.required.length !== 0) msg += (`**Bot Permissions:** \`${command.required.join("`, `")}\`\n`)
+            if (command.user && command.user.length !== 0) msg += (`**User Permissions:** \`${command.user.join("`, `")}\`\n`)
+            if (command.usage) msg += (`**Usage:** \`${PREFIX}${command.usage}\`\n`);
+            if (command.premium) msg += (`**Premium:** ${command.premium}\n`)
+
+            let helpEmbed = new Discord.MessageEmbed()
+                .setTitle(":books: Command Help")
+                .setDescription(msg)
+                .setColor(client.colors.sky)
+                .setFooter(message.client.user.username, message.client.user.displayAvatarURL())
+                .addField('\u200b', '[Invite Me](https://discord.com/oauth2/authorize?client_id=800549820485599272&scope=bot&permissions=2080768255) ● Support Server coming Soon')
+            return message.channel.send(helpEmbed)
         }
-        let msg = ""
-        msg += (`**Name:** ${command.name}\n`);
-
-        if (command.aliases && command.aliases.length !== 0) msg += (`**Aliases:** ${command.aliases.join(", ")}\n`);
-        if (command.description) msg += (`**Description:** ${command.description}\n`);
-        if (command.required && command.required.length !== 0) msg += (`**Bot Permissions:** \`${command.required.join("`, `")}\`\n`)
-        if (command.user && command.user.length !== 0) msg += (`**User Permissions:** \`${command.user.join("`, `")}\`\n`)
-        if (command.usage) msg += (`**Usage:** \`${PREFIX}${command.usage}\`\n`);
-        if (command.premium) msg += (`**Premium:** ${command.premium}\n`)
-
-        let helpEmbed = new Discord.MessageEmbed()
-            .setTitle(":books: Command Help")
-            .setDescription(msg)
-            .setColor(client.colors.sky)
-            .setFooter(message.client.user.username, message.client.user.displayAvatarURL())
-            .addField('\u200b', '[Invite Me](https://discord.com/oauth2/authorize?client_id=800549820485599272&scope=bot&permissions=2080768255) ● Support Server coming Soon')
-return message.channel.send(helpEmbed)
     }
 }
