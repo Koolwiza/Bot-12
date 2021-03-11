9798d6c0bc121b7b3c59f062297655bcb137a23d
Koolwiza <72822494+Koolwiza@users.noreply.github.com>
2021-03-11 10:04:37 -0800

    New auto docs updater

--- scripts/autoUpdateDocs.js
+++ scripts/autoUpdateDocs.js
@@ -6,7 +6,7 @@ module.exports = async (client) => {
 
         let categories = []
 
-        let docs = "# Bot-12 Command List\n"
+        let docs = "# Bot-12 Command List\n> :heart: Command list generated [here](https://github.com/Koolwiza/Bot-12/blob/main/scripts/autoUpdateDocs.js)\n"
 
         commands.forEach(command => {
             if (!categories.includes(command.category)) {
@@ -14,22 +14,49 @@ module.exports = async (client) => {
             }
         })
 
-        await categories.forEach(cat => {
+        categories.forEach(cat => {
             const tCommands = commands.filter(cmd => cmd.category === cat)
             docs += `
             \n
-## ${cat} [${tCommands.size}]
-    
-| Name | Description | Usage
-| ---- | ----------- | ----- \n
-${tCommands.map(command => `| ${command.name} | ${command.description} | ${command.usage.replace(/\|/g, "\\|").replace(/\</g, "\\<").replace(/\>/g, "\\>")}`).join("\n")}`
+### ${cat} [${tCommands.size}] 
+
+| Name | Description | 
+| ---- | ----------- | 
+${tCommands.map(command => {
+    return `| [${command.name}](https://github.com/Koolwiza/Bot-12/blob/master/docs/commands.md#${command.name}) | ${command.description} | `
+}).join("\n")}\n\n`
         })
+
+        docs += "# Detailed Command List"
+
+        categories.forEach(cat => {
+            const tCommands = commands.filter(cmd => cmd.category === cat)
+
+            docs += `
+## ${cat} | ${tCommands.size} Commands
+
+${tCommands.map(command => {
+   return `
+
+### ${command.name.toProperCase()}
+
+Command: ${command.name}\\
+Description: ${command.description}\\
+Usage: ${command.usage}\\
+Aliases: ${command.aliases.join(', ')}
+
+[Back to top](https://github.com/Koolwiza/Bot-12/blob/master/docs/commands.md#bot-12-command-list)`
+}).join("\n\n")}`
+        })
+
+
+
         const fs = require('fs')
-        if(fs.existsSync('./docs/command-list.md')) {
-            fs.writeFileSync('./docs/command-list.md', docs.trim())
+        if (fs.existsSync('./docs/commands.md')) {
+            fs.writeFileSync('./docs/commands.md', docs.trim())
             client.logger.log("Command list updated!")
         }
-        
+
     }, 10 * 60 * 1000)
 
 }
