5de7537202a7b8741f3dd325a4cbd9ff1033d9d4
Koolwiza <72822494+Koolwiza@users.noreply.github.com>
2021-03-11 10:04:21 -0800

    :fire: Removed array extenders

--- helpers/extenders.js
+++ helpers/extenders.js
@@ -69,37 +69,6 @@ MessageEmbed.prototype.error = function () {
 }
 
 /**
- * Array prototype functions
- */
-
-Array.prototype.random = function () {
-	return this[Math.floor(Math.random() * this.length)];
-};
-
-Array.prototype.remove = function (a) {
-	const index = this.indexOf(a);
-	if (index > -1) {
-		this.splice(index, 1);
-	}
-	return this
-}
-
-Array.prototype.shuffle = function () {
-	const array = [];
-	this.forEach(element => array.push(element));
-	let currentIndex = array.length,
-		temporaryValue, randomIndex;
-	while (0 !== currentIndex) {
-		randomIndex = Math.floor(Math.random() * currentIndex);
-		currentIndex -= 1;
-		temporaryValue = array[currentIndex];
-		array[currentIndex] = array[randomIndex];
-		array[randomIndex] = temporaryValue;
-	}
-	return array;
-}
-
-/**
  * String prototype functions
  * Generally bad practice to attach functions to native types
  * Just that this function is useful
