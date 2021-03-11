0ef5e43982feb2405a4a7b38c96df71d19501eed
Koolwiza <72822494+Koolwiza@users.noreply.github.com>
2021-03-11 10:03:05 -0800

    Fixed wording

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -30,7 +30,7 @@ module.exports = async (client, member) => {
         await owner.send(
           new Discord.MessageEmbed()
           .setTitle(":warning: Bot added")
-          .setDescription(`${user.toString()} has added the bot ${bot.toString()} to your server. This user does not have the modrole. The bot has been kicked `)
+          .setDescription(`${user.toString()} has added the bot ${bot.toString()} to your server. This user is not whitelisted. The bot has been kicked `)
           .setColor(client.colors.red)
           .setFooter(client.user.username, client.user.displayAvatarURL())
         )
