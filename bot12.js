8a46e0bd779c8ccc207f9c7d25b5bf2dae2f6c77
Koolwiza <72822494+Koolwiza@users.noreply.github.com>
2021-03-31 09:58:28 -0700

    Graph of members on join command

--- bot12.js
+++ bot12.js
@@ -4,14 +4,21 @@ const {
   cmdloader = require('./scripts/cmdloader'),
   eventloader = require('./scripts/eventloader'),
   docsUpdater = require('./scripts/autoUpdateDocs'),
-  Bot12 = require('./src/struct/Bot12Client'),
+  Bot12 = require('./src/struct/Bot12'),
   client = new Bot12({
     partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
     restTimeOffset: 250,
     ws: {
       intents: Intents.ALL
     }
-  })
+  }),
+  {
+      CronJob
+  } = require('cron')
+
+const job = new CronJob('0 12 * * Sun', () => {
+  client.joins.clear()
+}, null, true, 'America/Los_Angeles')
 
 client.on("disconnect", () => client.logger.warn("Bot is disconnecting..."))
   .on("reconnecting", () => client.logger.log("Bot reconnecting..."))
