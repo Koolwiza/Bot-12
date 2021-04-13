a80f27018736f2cd1052498eaff8859588ab72e6
Koolwiza <72822494+Koolwiza@users.noreply.github.com>
2021-04-13 15:06:14 -0700

    Changed default cooldown from 3 -> 5

--- events/message.js
+++ events/message.js
@@ -81,7 +81,7 @@ module.exports = async (client, message) => {
 
   const now = Date.now();
   const timestamps = cooldowns.get(command.name);
-  const cooldownAmount = (command.cooldown || 3) * 1000;
+  const cooldownAmount = (command.cooldown || 5) * 1000;
 
   if (timestamps.has(message.author.id)) {
     const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
