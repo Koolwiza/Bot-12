24c0528f6404eff46c7faa589a37726bd3ee33d0
Koolwiza <72822494+Koolwiza@users.noreply.github.com>
2021-04-17 10:30:44 -0700

    Make it check for if people DON'T have modrole is stead of if they do :flushed: and make number have members

--- commands/Administrator/roles.js
+++ commands/Administrator/roles.js
@@ -8,20 +8,21 @@ module.exports = {
   required: [],
   user: [],
   category: __dirname.split("commands\\")[1],
-  
+
   premium: false,
   guildOnly: false,
   ignore: true,
   async execute(message, args, client, data) {
-    if(!message.member.permissions.has(Discord.Permissions.FLAGS.MANAGE_ROLES) || !client.modRole(message, data.guild)) return client.missingArgs(message, ["MANAGE_SERVER"])
+    if (!message.member.permissions.has(Discord.Permissions.FLAGS.MANAGE_ROLES) || !client.modRole(message, data.guild)) return client.missingArgs(message, ["MANAGE_SERVER"])
     let msg = "```asciidoc\n== ROLES ==\n"
     let names = message.guild.roles.cache.map(c => `${c.name}`)
     let longest = names.reduce((long, str) => Math.max(long, str.length), 0)
     message.guild.roles.cache.forEach(c => {
-        
-        msg+= `${c.name}${" ".repeat(longest - c.name.length)} :: ${c.members.size}\n`
+      msg += `${c.name}${" ".repeat(longest - c.name.length)} :: ${c.members.size} members\n`
     })
     msg += "```"
-    message.channel.send(msg, {split: true})
+    message.channel.send(msg, {
+      split: true
+    })
   }
 }
