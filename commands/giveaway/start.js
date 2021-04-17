0b74370f3f26af2c6d8a6eadce00a99b2f11c934
Crypto <73986063+Crypto195@users.noreply.github.com>
2021-04-17 09:35:41 +0530

    Update start.js

--- commands/giveaway/start.js
+++ commands/giveaway/start.js
@@ -7,7 +7,9 @@ module.exports = {
         category: "giveaway",
         aliases: ["giveaway-start"],
         usage: '<channel> <duration> <winners>, <prize>',
-    run: async (bot, message, args) => {
+    premium: false,
+         guildOnly: false,
+         async execute(message, args, client, data) {
        if(!message.member.hasPermission('MANAGE_MESSAGES') && !message.member.roles.cache.some((r) => r.name === "Giveaways")){
         return message.channel.send(':x: You need to have the manage messages permissions to start giveaways.');
     }
