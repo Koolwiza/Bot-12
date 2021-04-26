66465b46e604836f7b58cad05f2028b584bd2907
Koolwiza <72822494+Koolwiza@users.noreply.github.com>
2021-04-25 19:43:18 -0700

    N̊͐̏eͨͪͪw̑͢͝ ͦ̊̓c̅̉̓o̡̿̔m̔ͤ̾mͯͯ̿a̠̠ͥnͭͮ̓d̢̓ͣ

---
+++ commands/Fun/zalgo.js
@@ -0,0 +1,32 @@
+const {
+    zalgo
+} = require('@favware/zalgo')
+const Discord = require('discord.js'),
+    Bot12 = require('../../src/struct/Bot12')
+
+module.exports = {
+    name: 'zalgo',
+    description: 'Turn your text into evil characters!',
+    usage: 'zalgo <text>',
+    aliases: [],
+    required: [],
+    user: [],
+    category: __dirname.split("commands\\")[1],
+
+    premium: false,
+    guildOnly: false,
+
+    /**
+     * 
+     * @param {Discord.Message} message 
+     * @param {Array} args 
+     * @param {Bot12} client 
+     * @param {object} data 
+     */
+    async execute(message, args, client, data) {
+       let text = args.join(" ")
+       if(!text) return message.error("Please provide text to convert")
+       let convertedText = zalgo(text)
+       return message.sendE("ZALGO 😈", convertedText, client.colors.red)
+    }
+}
