af3351f1738629012531fe26578cffa6d24db9eb
Koolwiza <72822494+Koolwiza@users.noreply.github.com>
2021-04-26 17:34:47 -0700

    Youtube command

--- commands/General/youtube.js
+++ commands/General/youtube.js
@@ -1,7 +1,8 @@
 const base = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&order=viewCount'
 
 const Discord = require('discord.js'),
-    Bot12 = require('../../src/struct/Bot12')
+    Bot12 = require('../../src/struct/Bot12'),
+    fetch = require('node-fetch')
 
 module.exports = {
     name: 'youtube',
@@ -21,6 +22,22 @@ module.exports = {
      * @param {object} data 
      */
     async execute(message, args, client, data) {
-       let query = args.join(" ")
+        let query = args.join(" ")
+        if (!query) return message.error("Please provide a search query")
+        query = encodeURIComponent(query)
+        let body = await fetch(`${base}&key=${client.config.google_api_key}&q=${query}`).then(res => res.json())
+        let video = body.items[0]
+
+        if(!video) return message.error("No videos found with that query")
+        let embed = new Discord.MessageEmbed()
+            .setTitle(video.snippet.title)
+            .setDescription(video.snippet.description)
+            .setImage(video.snippet.thumbnails.high.url)
+            .setColor(client.colors.red)
+            .setAuthor(video.snippet.channelTitle, "", `https://www.youtube.com/channel/${video.snippet.channelId}`)
+            .setURL(`https://www.youtube.com/watch?v=${video.id.videoId}`)
+        return message.channel.send("The results are based on highest views", {
+            embed: embed
+        })
     }
 }
